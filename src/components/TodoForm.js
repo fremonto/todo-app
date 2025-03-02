be768f6b2e38ad8187e67f87b9aadb2fc9468c16
Olivier Frémont <86167108+fremonto@users.noreply.github.com>
2025-03-02 15:58:49 +0100

    Mise à jour du formulaire pour inclure la priorité

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -3,11 +3,13 @@ import './TodoForm.css';
 
 function TodoForm({ addTodo }) {
   const [input, setInput] = useState('');
+  const [priority, setPriority] = useState('normale');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    addTodo(input);
+    addTodo(input, priority);
     setInput('');
+    setPriority('normale');
   };
 
   return (
@@ -19,6 +21,15 @@ function TodoForm({ addTodo }) {
         onChange={(e) => setInput(e.target.value)}
         className="todo-input"
       />
+      <select 
+        value={priority} 
+        onChange={(e) => setPriority(e.target.value)}
+        className="priority-select"
+      >
+        <option value="basse">Priorité basse</option>
+        <option value="normale">Priorité normale</option>
+        <option value="haute">Priorité haute</option>
+      </select>
       <button type="submit" className="todo-button">
         Ajouter
       </button>
